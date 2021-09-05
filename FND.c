0bb6070d35c2ffed681c4df0a3a62d9b35a26d6e
Rama054 <lopez.rami@hotmail.com>
2021-09-05 04:53:40 -0300

    Automata hecho, VER IGUALMENTE

--- FND.c
+++ FND.c
@@ -32,12 +32,70 @@ void mostrar(nodoL* L){
   }
 	return;
 }
- 
+
+int Columna (char c) {
+	switch (c) {
+		case '+': 
+    case '-':
+      return 0;
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+      return 1;
+    case 'A':
+    case 'B':
+    case 'C':
+    case 'D':
+    case 'E':
+    case 'F':
+    case 'a':
+    case 'b':
+    case 'c':
+    case 'd':
+    case 'e':
+    case 'f':
+      return 2;
+    case 'x':
+      return 3;
+    case '0':
+      return 4;
+    case '8':
+    case '9':
+      return 5;
+    default:
+      return 6;
+	}
+}
+
+int EsPalabra2 (const char *s) { 
+	static int tt [7][7] = {{1,2,6,6,3,2,6}, /* Tabla de Transiciones */
+							            {6,2,6,6,6,2,6},
+						            	{6,2,6,6,2,2,6},
+						            	{6,4,6,5,6,6,6},
+                          {6,4,6,6,4,6,6},
+                          {6,5,5,6,5,5,6},
+                          {6,6,6,6,6,6,6},
+                          };
+	int e=0; 
+	unsigned int i; 
+	for (i=0; s[i]!='\0' && e!=6; i++)
+	{
+		e = tt [e][Columna(s[i])];
+    printf("%d\n",e);
+	}	
+		
+	return e!=6; /* estado final? retorna 1 */
+} 
+
 
 int main(int argc, char *argv[])
 {
     char cadenaPrincipal[80];
-    lista raiz = NULL;
+    lista cadenas = NULL;
 
     scanf("%s",cadenaPrincipal);
     char delimitador[] = "&";
@@ -45,11 +103,28 @@ int main(int argc, char *argv[])
   	token = strtok(cadenaPrincipal, delimitador);
     while(token!= NULL) 
    	{
-      insertarEnLista(&raiz,token);
+      insertarEnLista(&cadenas,token);
       token = strtok(NULL, delimitador);
     }
-    mostrar(raiz);
+
+    if(EsPalabra2(cadenaPrincipal))
+      printf("La palabra pertenece al lenguaje\n");
+      else
+      printf("La palabra no pertenece al lenguaje\n");
+
+
+
+
+
+
+
+
+
+    //mostrar(cadenas);
 
 
 
 }
+
+
+
